f159d5d0d680fa6653e2d8afb3fe83f068a1b7d1
ixi <paulenka.aleh@gmail.com>
2014-02-11 06:05:31 +0400

    Implementing user removal.

--- src/main/java/paulenka/aleh/wordbook/action/user/DeleteUserAction.java
+++ src/main/java/paulenka/aleh/wordbook/action/user/DeleteUserAction.java
@@ -1,22 +1,20 @@
 package paulenka.aleh.wordbook.action.user;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import paulenka.aleh.wordbook.dao.UserDao;
-import paulenka.aleh.wordbook.entity.User;
 import paulenka.aleh.wordbook.interceptor.security.Authorization;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 @Authorization
-public class ListUserAction extends ActionSupport {
+public class DeleteUserAction extends ActionSupport {
 
     private static final long serialVersionUID = 1L;
 
     private UserDao userDao;
 
-    private List<User> users;
+    private int id;
 
     protected UserDao getUserDao() {
         if (userDao == null) {
@@ -25,18 +23,18 @@ public class ListUserAction extends ActionSupport {
         return userDao;
     }
 
-    public List<User> getUsers() {
-        return users;
+    public int getId() {
+        return id;
     }
 
-    public void setUsers(List<User> users) {
-        this.users = users;
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
     public String execute() {
         try {
-            setUsers(getUserDao().list());
+            getUserDao().delete(getId());
             return SUCCESS;
         } catch (SQLException ex) {
             // TODO: Redirect to 500 page
--- src/main/java/paulenka/aleh/wordbook/dao/UserDao.java
+++ src/main/java/paulenka/aleh/wordbook/dao/UserDao.java
@@ -14,50 +14,55 @@ import paulenka.aleh.wordbook.util.HashingUtil;
 
 public class UserDao extends JdbcDaoTemplate {
 
-	private final static String QUERY_LOGIN = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.USERNAME + "` = ? AND `" + UserTable.PASSWORD + "` = ? LIMIT 1;";
-	private final static String QUERY_IS_USER_EXISTS = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.USERNAME + "` = ? LIMIT 1;";
-	private final static String QUERY_CREATE_USER = "INSERT INTO `" + UserTable.TABLE + "` (`" + UserTable.USERNAME + "`, `" + UserTable.PASSWORD + "`) VALUES (?, ?);";
-	private final static String QUERY_GET_USER = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.ID + "` = ? LIMIT 1;";
-	private final static String QUERY_LIST_USERS = "SELECT * FROM `" + UserTable.TABLE + "`;";
+    private final static String QUERY_LOGIN = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.USERNAME + "` = ? AND `" + UserTable.PASSWORD + "` = ? LIMIT 1;";
+    private final static String QUERY_IS_USER_EXISTS = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.USERNAME + "` = ? LIMIT 1;";
+    private final static String QUERY_CREATE_USER = "INSERT INTO `" + UserTable.TABLE + "` (`" + UserTable.USERNAME + "`, `" + UserTable.PASSWORD + "`) VALUES (?, ?);";
+    private final static String QUERY_DELETE_USER = "DELETE FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.ID + "` = ? LIMIT 1;";
+    private final static String QUERY_GET_USER = "SELECT * FROM `" + UserTable.TABLE + "` WHERE `" + UserTable.ID + "` = ? LIMIT 1;";
+    private final static String QUERY_LIST_USERS = "SELECT * FROM `" + UserTable.TABLE + "`;";
 
-	private UserMapper userMapper;
+    private UserMapper userMapper;
 
-	public UserMapper getUserMapper() {
-		if (userMapper == null) {
-			userMapper = new UserMapper();
-		}
-		return userMapper;
-	}
+    public UserMapper getUserMapper() {
+        if (userMapper == null) {
+            userMapper = new UserMapper();
+        }
+        return userMapper;
+    }
 
-	public User login(Credentials credentials) throws SQLException {
-		try {
-			String username = credentials.getUsername();
-			byte[] passwordHash = HashingUtil.sha2(credentials.getPassword());
+    public User login(Credentials credentials) throws SQLException {
+        try {
+            String username = credentials.getUsername();
+            byte[] passwordHash = HashingUtil.sha2(credentials.getPassword());
 
-			return executeQueryWithSingleResult(getUserMapper(), QUERY_LOGIN, username, passwordHash);
-		} catch (NoSuchAlgorithmException ex) {
-			throw new SQLException(ex);
-		}
-	}
+            return executeQueryWithSingleResult(getUserMapper(), QUERY_LOGIN, username, passwordHash);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new SQLException(ex);
+        }
+    }
 
-	public boolean isUserExists(String username) throws SQLException {
-		return executeQueryWithSingleResult(getUserMapper(), QUERY_IS_USER_EXISTS, username) != null;
-	}
+    public boolean isUserExists(String username) throws SQLException {
+        return executeQueryWithSingleResult(getUserMapper(), QUERY_IS_USER_EXISTS, username) != null;
+    }
 
-	public User register(Registration registration) throws SQLException {
-		try {
-			String username = registration.getUsername();
-			byte[] passwordHash = HashingUtil.sha2(registration.getPassword());
+    public User register(Registration registration) throws SQLException {
+        try {
+            String username = registration.getUsername();
+            byte[] passwordHash = HashingUtil.sha2(registration.getPassword());
 
-			int id = executeInsert(QUERY_CREATE_USER, username, passwordHash);
-			return executeQueryWithSingleResult(getUserMapper(), QUERY_GET_USER, id);
-		} catch (NoSuchAlgorithmException ex) {
-			throw new SQLException(ex);
-		}
+            int id = executeInsert(QUERY_CREATE_USER, username, passwordHash);
+            return executeQueryWithSingleResult(getUserMapper(), QUERY_GET_USER, id);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new SQLException(ex);
+        }
 
-	}
+    }
 
-	public List<User> list() throws SQLException {
-		return executeQuery(getUserMapper(), QUERY_LIST_USERS);
-	}
+    public List<User> list() throws SQLException {
+        return executeQuery(getUserMapper(), QUERY_LIST_USERS);
+    }
+
+    public void delete(int id) throws SQLException {
+        executeUpdate(QUERY_DELETE_USER, id);
+    }
 }

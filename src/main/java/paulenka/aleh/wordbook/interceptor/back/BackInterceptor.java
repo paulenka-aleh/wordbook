d6138555c12509cd32c31a4a3ff3bc08f55d791c
ixi <paulenka.aleh@gmail.com>
2014-02-11 06:35:03 +0400

    Fixing problem with get uri params and post uri back redirecting.

--- src/main/java/paulenka/aleh/wordbook/interceptor/back/BackInterceptor.java
+++ src/main/java/paulenka/aleh/wordbook/interceptor/back/BackInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.ServletActionContext;
 
+import paulenka.aleh.wordbook.constant.HttpRequestMethod;
 import paulenka.aleh.wordbook.constant.SessionAttribute;
 import paulenka.aleh.wordbook.util.ActionAnnotationUtil;
 
@@ -17,53 +18,57 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
 public class BackInterceptor extends AbstractInterceptor {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String BACK_ATTRIBUTE = "back";
-	private static final String DEFAULT_BACK_URI = "/";
+    private static final String BACK_ATTRIBUTE = "back";
+    private static final String DEFAULT_BACK_URI = "/";
 
-	@SuppressWarnings("unchecked")
-	protected Deque<String> getBackUriStack(ActionInvocation invocation) {
-		Map<String, Object> session = invocation.getInvocationContext().getSession();
-		if (!session.containsKey(SessionAttribute.BACK_URI_STACK)) {
-			session.put(SessionAttribute.BACK_URI_STACK, new ArrayDeque<>());
-		}
-		return (Deque<String>) session.get(SessionAttribute.BACK_URI_STACK);
-	}
+    @SuppressWarnings("unchecked")
+    protected Deque<String> getBackUriStack(ActionInvocation invocation) {
+        Map<String, Object> session = invocation.getInvocationContext().getSession();
+        if (!session.containsKey(SessionAttribute.BACK_URI_STACK)) {
+            session.put(SessionAttribute.BACK_URI_STACK, new ArrayDeque<>());
+        }
+        return (Deque<String>) session.get(SessionAttribute.BACK_URI_STACK);
+    }
 
-	protected boolean isBackAction(ActionInvocation invocation) {
-		BackResultAction classAnnotation = ActionAnnotationUtil.getActionClassAnnotation(invocation, BackResultAction.class);
-		BackResultAction methodAnnotation = ActionAnnotationUtil.getActionClassAnnotation(invocation, BackResultAction.class);
+    protected boolean isBackAction(ActionInvocation invocation) {
+        BackResultAction classAnnotation = ActionAnnotationUtil.getActionClassAnnotation(invocation, BackResultAction.class);
+        BackResultAction methodAnnotation = ActionAnnotationUtil.getActionClassAnnotation(invocation, BackResultAction.class);
 
-		return classAnnotation != null || methodAnnotation != null;
-	}
+        return classAnnotation != null || methodAnnotation != null;
+    }
 
-	protected String getCurrentUri() {
-		HttpServletRequest request = ServletActionContext.getRequest();
-		return request.getRequestURI().substring(request.getContextPath().length());
-	}
+    protected String getCurrentUri() {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        String requesrUri = request.getRequestURI().substring(request.getContextPath().length());
+        return requesrUri + '?' + request.getQueryString();
+    }
 
-	@Override
-	public String intercept(ActionInvocation invocation) throws Exception {
-		if (getBackUriStack(invocation).isEmpty()) {
-			getBackUriStack(invocation).push(DEFAULT_BACK_URI);
-		}
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
 
-		invocation.getStack().set(BACK_ATTRIBUTE, getBackUriStack(invocation).getFirst());
+        if (getBackUriStack(invocation).isEmpty()) {
+            getBackUriStack(invocation).push(DEFAULT_BACK_URI);
+        }
 
-		if (!isBackAction(invocation)) {
-			getBackUriStack(invocation).clear();
-		}
+        invocation.getStack().set(BACK_ATTRIBUTE, getBackUriStack(invocation).getFirst());
 
-		getBackUriStack(invocation).remove(getCurrentUri());
-		getBackUriStack(invocation).push(getCurrentUri());
+        if (HttpRequestMethod.GET.equalsIgnoreCase(ServletActionContext.getRequest().getMethod())) {
+            if (!isBackAction(invocation)) {
+                getBackUriStack(invocation).clear();
+            }
 
-		String result = invocation.invoke();
+            getBackUriStack(invocation).remove(getCurrentUri());
+            getBackUriStack(invocation).push(getCurrentUri());
+        }
 
-		if (isBackAction(invocation) && Action.SUCCESS.equals(result)) {
-			getBackUriStack(invocation).pop();
-		}
+        String result = invocation.invoke();
 
-		return result;
-	}
+        if (isBackAction(invocation) && Action.SUCCESS.equals(result)) {
+            getBackUriStack(invocation).pop();
+        }
+
+        return result;
+    }
 }

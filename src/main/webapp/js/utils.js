fc61a8888f69a853a43f3b3b7c7ff8b344c2b1e7
Aleh Paulenka <paulenka.aleh@gmail.com>
2014-02-11 05:20:35 -0430

    Adding  method to fill height.

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -4,3 +4,24 @@ String.prototype.format = function () {
 		return args[n];
 	});
 };
+
+(function($) {
+	$.fn.fill = function() {
+
+		var parent = this.parent();
+		var prev = this.prevAll();
+		var next = this.nextAll();
+
+		var height = function(element) { return $(element).outerHeight(true); };
+		var sum = function(previous, current) { return previous + current; };
+
+		var before = $.map(prev, height).reduce(sum);
+		var after = $.map(next, height).reduce(sum);
+
+		parent.css({ 'padding-top' : before, 'padding-bottom' : after });
+		this.css({ 'height' : '100%' });
+		$(parent.children(':first-child')).css({ 'margin-top' : -before });
+
+		return this;
+	};
+})(jQuery);

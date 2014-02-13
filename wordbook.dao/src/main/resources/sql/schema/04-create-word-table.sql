87424ccedf384de3ab938e0532ce1838863c766e
Aleh Paulenka <paulenka.aleh@gmail.com>
2014-02-13 07:47:05 -0430

    Adding Efremova disctionary and making appropriate schema changes.

--- wordbook.dao/src/main/resources/sql/schema/04-create-word-table.sql
+++ wordbook.dao/src/main/resources/sql/schema/04-create-word-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `word` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `explanation` varchar(4096) NOT NULL,
+  `explanation` varchar(16384) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
